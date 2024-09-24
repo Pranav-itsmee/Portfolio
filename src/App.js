fbdf35530b199258838f8bd2dd4d7db21ffefafe
Pranav <160521493+Pranav-itsmee@users.noreply.github.com>
2024-09-24 11:58:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from "./utils/Themes";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Route , Routes } from "react-router-dom";
+import Hero from "./components/sections/Hero";
+import Skills from "./components/sections/Skills";
+import Experience from "./components/sections/Experience";
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+  width: 100%;
+  height: 90vh;
+  overflow-x: hidden;
+  position: relative;
+`;
+
+function App() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <BrowserRouter>
+        <Navbar />
+        <Body>
+          <Hero/>
+          <Skills/>
+          <Experience />
+        </Body>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
